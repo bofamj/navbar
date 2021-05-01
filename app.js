66da98ecf7e1a565585c3475844185cddf3f007f
mustafa <mustafamjaber@yahoo.com>
2021-05-01 16:03:09 +0200

    finishing the app

---
+++ app.js
@@ -0,0 +1,12 @@
+
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav');
+
+
+
+burger.addEventListener('click', function() {
+    nav.classList.toggle('navApp')
+});
+
+
+
